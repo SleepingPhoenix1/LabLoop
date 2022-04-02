6e3e6ae40437929b47f13c61dfc8ae85ac2a0bea
SleepingPhoenix1 <SleepingPhoenix2@gmail.com>
2022-04-02 21:10:28 +0300

    added pixel art and export

--- scripts/entity/Enemy_pl.gd
+++ scripts/entity/Enemy_pl.gd
@@ -1,24 +1,49 @@
 extends KinematicBody2D
 
 var velocity = Vector2()
-var speed = 20
+export var speed = 20
 var gravity = 10
+var sleeping = true
 
 func _physics_process(delta):
 	if $raycasts/RayCastLeft.is_colliding() == true:
-		velocity.x = -speed
+		if sleeping:
+			$AnimationPlayer.play("wake_up")
+		else: 
+			$AnimationPlayer.play("walk")
+			velocity.x = -speed
 	elif $raycasts/RayCastRight.is_colliding() == true:
-		velocity.x = speed
+		
+		if sleeping:
+			$AnimationPlayer.play("wake_up")
+		else: 
+			$AnimationPlayer.play("walk")
+			velocity.x = speed
+		
 	else: 
 		velocity.x = 0
+		if $AnimationPlayer.current_animation == "walk":
+			$SleepTimer.start()
+			$AnimationPlayer.play("idle")
 	
 	if !is_on_floor():
 		velocity.y += gravity
 	else:
 		velocity.y = 1
 	move_and_slide(velocity, Vector2.UP)
-
+	$Robot.flip_h = true if velocity.x <0 else false
 
 func _on_DamageArea_body_entered(body):
 	get_tree().reload_current_scene()
 	
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "wake_up":
+		sleeping = false
+
+
+func _on_SleepTimer_timeout():
+	$AnimationPlayer.play("to_sleep")
+	sleeping = true
+	print("F")
